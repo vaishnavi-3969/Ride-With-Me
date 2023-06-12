98db7ef304864df5f38f73819eae3b5f8192e2f4
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-06-12 17:02:57 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Homepage from './components/Homepage';
+import ContactUs from './components/ContactUs';
+import Messaging from './components/Messaging';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>s
+        <Route path='/' element={<Homepage/>} exact/>
+        <Route path='/homepage' element={<Homepage/>} exact/>
+        <Route path='/contactUs' element ={<ContactUs/>} exact/>
+        <Route path='/messaging' element={<Messaging/>} exact/>
+        <Route path='/userProfile' element={<UserProfile/>} exact/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
